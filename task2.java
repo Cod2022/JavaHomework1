7e983c2d1768ac9f334a503fa82838b6364a6a20
My Name <Me@gmail.com>
2022-11-21 21:46:10 +0400

    2 task started

---
+++ task2.java
@@ -0,0 +1,9 @@
+// Вывести все простые числа от 1 до 1000
+
+public class task2 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 1000; i++) {
+            if (i % 2 == 1) System.out.println(i);
+        }
+    }
+}
