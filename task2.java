2b1c704bfa00d8cf0de4798eb95b22c37374b756
My Name <Me@gmail.com>
2022-11-24 15:13:36 +0400

    tasked 2 completed

--- task2.java
+++ task2.java
@@ -2,8 +2,19 @@
 
 public class task2 {
     public static void main(String[] args) {
-        for (int i = 1; i < 1000; i++) {
-            if (i % 2 == 1) System.out.println(i);
+        for (int i = 2; i < 1000; i++) {
+            Integer res = i;
+        for (int j = 2; j < i; j++) {
+            if (i % j == 0) {
+                res = null;
+                break;
+                }
+            }
+        if (res != null) {
+            System.out.println(res);
+                }
+            
+
         }
     }
 }
